1b750e6501cb71b6abafc93ddef8cc054027c008
ppieniaz <pawel.pieniazek@intel.com>
2016-01-25 14:40:28 +0100

    #98298 - first step to parametrize mod_tile

--- src/parameterize_style.cpp
+++ src/parameterize_style.cpp
@@ -11,46 +11,37 @@
 
 #include "parameterize_style.hpp"
 
-
-static void parameterize_map_language(mapnik::Map &m, char * parameter) { 
+static void parameterize_map_user(mapnik::Map &m, char * parameter) {
     unsigned int i;
-    char * data = strdup(parameter); 
-    char * tok; 
-    char name_replace[256]; 
-    
-    name_replace[0] = 0; 
-    syslog(LOG_DEBUG, "Internationalizing map to language parameter: %s", parameter); 
-    tok = strtok(data,","); 
-    if (!tok) return; //No parameterization given 
-    strncat(name_replace, ", coalesce(", 255); 
-    while (tok) { 
-        if (strcmp(tok,"_") == 0) { 
-            strncat(name_replace,"name,", 255); 
-        } else { 
-            strncat(name_replace,"tags->'name:", 255); 
-            strncat(name_replace, tok, 255); 
-            strncat(name_replace,"',", 255); 
-        } 
-        tok = strtok(NULL, ","); 
-        
-    }
-    free(data);
-    name_replace[strlen(name_replace) - 1] = 0; 
-    strncat(name_replace,") as name", 255); 
-    for (i = 0; i < m.layer_count(); i++) { 
+    char name_replace[256];
+
+    name_replace[0] = 0;
+    syslog(LOG_DEBUG, "Modifying map to user parameter: %s", parameter);
+    char * username = strdup(parameter);
+
+    //TODO: Check if data is valid and clear it
+
+    if (!username) return; //No parameterization given
+    strncat(name_replace, "WHERE feeder_id = ", 255);
+    strncat(name_replace, username, 255);
+    free(username);
+
+    name_replace[strlen(name_replace)] = 0;
+    strncat(name_replace," AND", 255);
+    for (i = 0; i < m.layer_count(); i++) {
 #if MAPNIK_VERSION >= 300000
         mapnik::layer& l = m.get_layer(i);
 #else
         mapnik::layer& l = m.getLayer(i);
 #endif
-        mapnik::parameters params = l.datasource()->params(); 
+        mapnik::parameters params = l.datasource()->params();
         if (params.find("table") != params.end()) {
             boost::optional<std::string> table = params.get<std::string>("table");
-            if (table && table->find(",name") != std::string::npos) {
+            if (table && table->find("WHERE") != std::string::npos) {
                 std::string str = *table;
-                size_t pos = str.find(",name"); 
-                str.replace(pos,5,name_replace); 
-                params["table"] = str; 
+                size_t pos = str.find("WHERE");
+                str.replace(pos,5,name_replace);
+                params["table"] = str;
 #if MAPNIK_VERSION >= 200200
                 l.set_datasource(mapnik::datasource_cache::instance().create(params));
 #else
@@ -61,14 +52,15 @@ static void parameterize_map_language(mapnik::Map &m, char * parameter) {
         
     } 
 }
-  
 
 parameterize_function_ptr init_parameterization_function(char * function_name) {
     syslog(LOG_INFO, "Loading parameterization function for %s", function_name);
     if (strcmp(function_name, "") == 0) {
         return NULL;
-    } else if (strcmp(function_name, "language") == 0) {
-        return parameterize_map_language;
+    //} else if (strcmp(function_name, "language") == 0) {
+    //    return parameterize_map_language;
+    } else if (strcmp(function_name, "user") == 0) {
+        return parameterize_map_user;
     } else {
         syslog(LOG_INFO, "WARNING: unknown parameterization function for %s", function_name);
     }
